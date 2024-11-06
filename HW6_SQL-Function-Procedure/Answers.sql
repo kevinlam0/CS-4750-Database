64f166b0999e68c187a23604b35ab8c53f942958
kevinlam0 <kevinclam02@gmail.com>
2024-11-06 13:19:11 -0500

    Finished HW before adding error handling

--- HW6_SQL-Function-Procedure/Answers.sql
+++ HW6_SQL-Function-Procedure/Answers.sql
@@ -37,7 +37,7 @@ BEGIN
 END;
 GO
 
--- Question 4
+-- Question 4 (Assumption: we are checking the total amount spent by a customer and not just of a single order)
 CREATE FUNCTION dbo.CheckDiscountEligibility (@customer_id INT)
 RETURNS INT
 AS
@@ -53,7 +53,7 @@ BEGIN
 END;
 GO
 
--- Question 5
+-- Question 5 (Assumption: we are checking the total stock quantity of a product and not just if the product has any stock)
 CREATE FUNCTION dbo.CheckProductAvailability (@product_id INT)
 RETURNS INT
 AS
@@ -68,3 +68,65 @@ END;
 GO
 
 -- Question 6
+CREATE PROCEDURE AddNewEmployee
+    @employee_id INT,
+    @first_name VARCHAR(100),
+    @last_name VARCHAR(100),
+    @base_salary DECIMAL(6, 2),
+    @bonus DECIMAL(6, 2),
+    @allowance DECIMAL(6, 2),
+    @department_id INT
+AS
+BEGIN
+    INSERT INTO Employee (employee_id, first_name, last_name, base_salary, bonus, allowance, department_id)
+    VALUES (@employee_id, @first_name, @last_name, @base_salary, @bonus, @allowance, @department_id);
+END;
+GO
+
+-- Question 7 (Do error handling | Assumption: we are only updating a single product based on the product id)
+CREATE PROCEDURE UpdateProductPrice
+    @product_id INT,
+    @new_price DECIMAL(6, 2)
+AS
+BEGIN
+    UPDATE Product
+    SET price = @new_price
+    WHERE product_id = @product_id;
+END;
+GO
+
+-- Question 8 (Assumption: we are archiving orders older than a certain date and deleting the data from the original table)
+CREATE PROCEDURE ArchiveOldOrders
+    @archive_date DATE
+AS
+BEGIN
+    INSERT INTO order_history (order_id, customer_id, order_date, total_amount)
+    SELECT order_id, customer_id, order_date, total_amount
+    FROM [Order]
+    WHERE order_date < @archive_date;
+
+    DELETE FROM [Order]
+    WHERE order_date < @archive_date;
+END;
+GO
+
+-- Question 9 (Do error handling)
+CREATE PROCEDURE ResetCustomerPassword
+    @customer_id INT,
+    @new_password VARCHAR(100)
+AS
+BEGIN
+    UPDATE Customer
+    SET password = @new_password
+    WHERE customer_id = @customer_id;
+END;
+GO
+
+-- Question 10 (Do error handling)
+CREATE PROCEDURE DeleteEmployee
+    @employee_id INT
+AS
+BEGIN
+    DELETE FROM Employee
+    WHERE employee_id = @employee_id;
+END;
